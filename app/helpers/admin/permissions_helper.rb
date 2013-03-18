18f3bf1d41fb09cee59ab1add0a4b6a23879d1b2
ch <cameron.hill@gmail.com>
2013-03-17 21:18:44 -0700

    Add ability to set user permissions to CRUD tickets in the admin panel

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -2,8 +2,9 @@ module Admin::PermissionsHelper
   def permissions
     {
       "view" => "View",
-      "edit" => "Edit",
-      "delete" => "Delete"
+      "create tickets" => "Create Tickets",
+      "edit tickets" => "Edit Tickets",
+      "delete tickets" => "Delete Tickets"
     }
   end
 end
