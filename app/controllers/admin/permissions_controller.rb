2d9795e20e8022331786527977daf489a94fa3d2
ch <cameron.hill@gmail.com>
2013-03-19 09:13:33 -0700

    Upgrade rails

--- app/controllers/admin/permissions_controller.rb
+++ app/controllers/admin/permissions_controller.rb
@@ -9,7 +9,7 @@ class Admin::PermissionsController < Admin::BaseController
   def update
     @user.permissions.clear
 
-    params[:permissions].each do |id, permissions|
+    params[:permissions] && params[:permissions].each do |id, permissions|
       project = Project.find(id)
       permissions.each do |permission, checked|
         Permission.create!(:user => @user, :thing => project, :action => permission)
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,10 +4,9 @@ class Project < ActiveRecord::Base
   has_many :tickets, :dependent => :delete_all
   has_many :permissions, :as => :thing
 
-  def self.viewable_by(user)
+  scope :viewable_by, lambda { |user|
     joins(:permissions).where(:permissions => { :action => "view",
-                                                :user_id => user.id })
-  end
+                                                :user_id => user.id }) }
 
   def self.for(user)
     user.admin ? Project : Project.viewable_by(user)
